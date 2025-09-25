4c3780acf8daa9d4ebb3a55aa01fc1d878e56b75
Vuk-Cug <cugdorja@tlu.ee>
2025-09-25 15:38:02 +0300

    w3_game

---
+++ w-3/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed := 100.0
+# @export allows for direct changes from the inspector
+@export var jump_speed := -300.0
+# pulls default grav from project settings
+var gravity :float= ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta: float) -> void:
+	var direction := Input.get_axis("move_left","move_right")
+	if direction != 0:
+		velocity.x=direction*speed
+	else:
+		velocity.x=move_toward(velocity.x,0,75)
+	if ! is_on_floor():
+		#delta makes it work regardless of FPS
+		velocity.y+=gravity*delta
+	if Input.is_action_just_pressed("jump") && is_on_floor():
+		velocity.y=jump_speed
+	move_and_slide()
