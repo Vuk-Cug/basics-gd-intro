61fd147460f61b5b603c7597cf8c03f7e9974c6e
Vuk-Cug <cugdorja@tlu.ee>
2025-11-22 14:07:47 +0200

    Vuk's things

--- final-project/d_4.gd
+++ final-project/d_4.gd
@@ -1,23 +1,30 @@
 #d_4 is the compass and level select
 extends Node2D
 @onready var animated_sprite : AnimatedSprite2D = $animated_sprite
-
 @export var level_names : Array[PackedScene]
 
 var levels := ["forest", "tundra", "desert", "sea"]
 @onready var scene_forest = preload("res://forest.tscn")
 @onready var scene_tundra = preload("res://tundra.tscn")
+#@onready var scene_tundra = preload("res://third.tscn")
+#@onready var scene_tundra = preload("res://fourth.tscn")
 
 var current_roll : int
 
 func _on_compass_pressed() -> void:
-	print("the compass spins...")
-	animated_sprite.play("Rolling")
+	play_animation()
+	roll_dice()
 
-func set_dice(new_number : int) -> void:
-	animated_sprite.play(levels[new_number -1])
-	current_roll = new_number
-	wait_and_change_scene()
+
+#when animation done
+#choose a random number between 1 and 4
+func roll_dice() -> void:
+	return randi_range(1, 4)
+
+	#load the scene for than number
+
+func play_animation() -> void:
+	animated_sprite.play("Rolling")
 
 func wait_and_change_scene():
 	await get_tree().create_timer(1).timeout
--- final-project/player.gd
+++ final-project/player.gd
@@ -7,9 +7,13 @@ extends CharacterBody2D
 # pulls default grav from project settings
 var gravity :float= ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready() -> void:
+	animated_sprite.play("idle")
+
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("move_left","move_right")
-	if direction != 0:
+	if input_event("move_left", "move_right"):
+		move_and_slide()
 		velocity.x=direction*speed
 	else:
 		velocity.x=move_toward(velocity.x,0,75)
@@ -18,16 +22,15 @@ func _physics_process(delta: float) -> void:
 		velocity.y+=gravity*delta
 	if Input.is_action_just_pressed("jump") && is_on_floor():
 		velocity.y=jump_speed
-	move_and_slide()
 	if direction >0:
 		animated_sprite.flip_h = false
 	elif direction <0:
 		animated_sprite.flip_h = true
 		
-	if is_on_floor():
-		if direction ==0:
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
+	#if is_on_floor():
+		#if direction ==0:
+			
+		#else:
+			#animated_sprite.play("run")
+	#else:
+		#animated_sprite.play("jump")
